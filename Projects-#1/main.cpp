e000187e2a13b6e81b2f0aaee1461bda0816ac49
Venom <81550434+DERBY-service@users.noreply.github.com>
2021-11-13 20:37:01 +0530

    ..

--- Projects-#1/main.cpp
+++ Projects-#1/main.cpp
@@ -8,12 +8,30 @@ using namespace std;
 int main(){
     
     string Name;
-    string Balance;
+    string Balance; //stores user balance
     int Dice; // stores the random number
     
     cout<<"\t ... Welcome to Venom Club ... "<<endl;
     cout<<" "<<endl;
     cout<<"Games Currently Available: "<<endl;
-    cout<<"1. Lucky number 7"<<endl;
+    cout<<"                          1. Lucky number 7"<<endl;
+    cout<<"                          Soon..."<<endl;
+
+    int yn; // whether user want to play or not
+    cout<<"Do you want to play (y/n): ";
+    cin>>yn;
+    if (yn == 'y' || yn == 'Y')
+    {
+        // code ==> user wants to play
+        cout<<"Enter your name: ";
+        getline(cin, Name);
+        cout<<"Hey"
+    }
+
+    if (yn == 'n' || yn == 'N')
+    {
+        return 1; // user exited the game
+    }
+        
     return 0;
 }
