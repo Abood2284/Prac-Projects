5b0564dacb2a0cc5c402bdd27ee673a9ee1cb675
Venom <81550434+DERBY-service@users.noreply.github.com>
2021-11-13 18:34:28 +0530

    Initial Commit

---
+++ Projects-#1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> // Needed to use strings
+#include <cstdlib> // Needed to use random numbers
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    
+    string Name;
+    string Balance;
+    int Dice; // stores the random number
+    
+    cout<<"\t ... Welcome to Venom Club ... "<<endl;
+    cout<<" "<<endl;
+    return 0;
+}
